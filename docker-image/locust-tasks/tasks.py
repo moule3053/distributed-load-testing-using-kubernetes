0d73c5e7c91b91887e26d90f68a2ed7859abbb93
Mulugeta Ayalew <mayalew@unknown3c970ebc3dc8.localdomain>
2017-12-27 22:33:32 +0100

    Customize settings

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,42 +1,98 @@
-#!/usr/bin/env python
-
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from realbrowserlocusts import HeadlessChromeLocust, PhantomJSLocust
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import random
 
 
-import uuid
+from locust import TaskSet, task
 
-from datetime import datetime
-from locust import HttpLocust, TaskSet, task
+baseUrl = "http://35.198.145.216/"
+products = ['beanie', 'belt', 'cap', 'sunglasses', 'hoodie-with-logo', 'hoodie-with-pocket', 'hoodie-with-zipper', 'hoodie', 'long-sleeve-tee', 'polo', 'tshirt', 'vneck-tshirt', 'woo-logo', 'woo-album-1', 'woo-album-2', 'woo-album-3', 'woo-single-1', 'woo-album-4', 'woo-single-2', 'woo-logo', 'premium-quality', 'ship-your-idea', 'ninja-silhouette', 'woo-ninja', 'happy-ninja', 'ship-your-idea', 'woo-ninja', 'patient-ninja', 'happy-ninja', 'ninja-silhouette', 'woo-logo', 'flying-ninja', 'premium-quality', 'woo-ninja']
+firstNames = ['Sara', 'Maria', 'Juicy', 'Meron','Christmas','Vanessa']
+lastNames = ['Michaels', 'Jordan', 'Clinton', 'Blunt', 'Harvey', 'Muller', 'Redfern']
 
+class LocustUserBehavior(TaskSet):
 
-class MetricsTaskSet(TaskSet):
-    _deviceid = None
+    def open_locust_homepage(self):
+        self.client.get(baseUrl)
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//a[text()="Shop"]')
+            )
+        )
 
-    def on_start(self):
-        self._deviceid = str(uuid.uuid4())
+    def click_through_to_shop(self):
+        self.client\
+            .find_element_by_xpath('//a[text()="Shop"]').click()
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//h1[text()="Shop"]')
+            )
+        )
+
+    def make_order_and_checkout(self):
+        self.client.get(baseUrl + "product/beanie")
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//h1[text()="Beanie"]')
+            )
+        )
+        self.client.find_element_by_name('add-to-cart').click()
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//a[text()="View cart"]')
+            )
+        )
+        self.client.get(baseUrl + "checkout")
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//h1[text()="Checkout"]')
+            )
+        )
+        self.client.find_element_by_id('billing_first_name').send_keys(random.choice(firstNames))
+        self.client.find_element_by_id('billing_last_name').send_keys(random.choice(lastNames))
+        self.client.find_element_by_id('billing_address_1').send_keys("Frankfurter Allee 223")
+        self.client.find_element_by_id('billing_postcode').send_keys("10367")
+        self.client.find_element_by_id('billing_city').send_keys("Berlin")
+        self.client.find_element_by_id('billing_email').send_keys("testing@yahoo.com")
+        self.client.find_element_by_id('place_order').click()
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//h1[text()="Order received"]')
+            )
+        )
+
 
     @task(1)
-    def login(self):
-        self.client.post(
-            '/login', {"deviceid": self._deviceid})
+    def homepage(self):
+        self.client.timed_event_for_locust(
+            "Go to", "homepage",
+            self.open_locust_homepage
+        )
 
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
+    @task(2)
+    def browse_products(self):
+        self.client.timed_event_for_locust(
+            "Click to",
+            "shop",
+            self.click_through_to_shop
+        )
 
+    @task(3)
+    def add_to_cart_and_checkout(self):
+        self.client.timed_event_for_locust(
+            "Add",
+            "product and checkout",
+            self.make_order_and_checkout
+        )
 
-class MetricsLocust(HttpLocust):
-    task_set = MetricsTaskSet
+class LocustUser(PhantomJSLocust):
+
+#    host = "not really used"
+#    timeout = 180  # in seconds in waitUntil thingies
+#    stop_timeout = 300
+#    min_wait = 0
+#    max_wait = 0
+    screen_width = 1200
+    screen_height = 1200
+    task_set = LocustUserBehavior
